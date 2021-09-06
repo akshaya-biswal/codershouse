0c862d4d7df194fbadb27f2c446a5fc6c162b0b4
Akshaya Biswal <akshaya.join@gmail.com>
2021-09-06 08:31:07 +0530

    Refresh Token and Logout api added

--- backend/controllers/auth-controller.js
+++ backend/controllers/auth-controller.js
@@ -81,6 +81,84 @@ class AuthController {
     const userDto = new UserDto(user);
     res.json({ auth: true, user: userDto });
   }
+
+  async refresh(req, res) {
+    // get refresh token from cookie
+    const { refreshToken: refreshTokenFromCookie } = req.cookies;
+
+    // check if token is valid
+    let userData;
+    try {
+      userData = await tokenService.verifyRefreshToken(
+        userData._id,
+        refreshTokenFromCookie
+      );
+
+      if (!userData) {
+        return res.status(401).json({ message: "Invalid token" });
+      }
+    } catch (error) {
+      return res.status(500).json({ message: "Internal error" });
+    }
+
+    // Check if token is in db
+    try {
+      const token = await tokenService.findRefreshToken(
+        userData._id,
+        refreshTokenFromCookie
+      );
+      if (!token) {
+        return res.status(401).json({ message: "Invalid token" });
+      }
+    } catch (err) {
+      return res.status(500).json({ message: "Internal error" });
+    }
+
+    // check if valid user
+    const user = await userService.findUser({ _id: userData._id });
+    if (!user) {
+      return res.status(404).json({ message: "No user" });
+    }
+
+    // Generate new tokens
+    const { refreshToken, accessToken } = tokenService.generateTokens({
+      _id: userData._id,
+    });
+
+    // Update refresh token
+    try {
+      await tokenService.updateRefreshToken(userData._id, refreshToken);
+    } catch (err) {
+      return res.status(500).json({ message: "Internal error" });
+    }
+
+    // put in cookie
+    res.cookie("refreshToken", refreshToken, {
+      maxAge: 1000 * 60 * 60 * 24 * 30,
+      httpOnly: true,
+    });
+
+    res.cookie("accessToken", accessToken, {
+      maxAge: 1000 * 60 * 60 * 24 * 30,
+      httpOnly: true,
+    });
+
+    // response
+    const userDto = new UserDto(user);
+    res.json({ user: userDto, auth: true });
+  }
+
+  // Logout
+  async logout() {
+    const { refreshToken } = req.cookies;
+    // delete refresh token from db
+    await tokenService.removeToken(refreshToken);
+    // delete cookies
+    res.clearCookie("refreshToken");
+    res.clearCookie("accessToken");
+    // response
+    res.json({ user: null, auth: false });
+  }
 }
 
 module.exports = new AuthController();
